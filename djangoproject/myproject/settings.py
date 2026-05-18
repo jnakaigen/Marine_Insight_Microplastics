28c71a8e9a10359017350bfb9bc29b4aab191961
jnakaigen <janakiratheesh2004@gmail.com>
2026-05-18 16:28:24 +0530

    Add missing TEMPLATES config for Django admin

--- djangoproject/myproject/settings.py
+++ djangoproject/myproject/settings.py
@@ -26,7 +26,21 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'corsheaders',
 ]
-
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
